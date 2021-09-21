46ed7abb92da22da4b67ecd3fe37b6d7448e5654
FarmVivi <farmvividev@gmail.com>
2021-09-21 11:26:29 +0200

    todo

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -5,6 +5,7 @@ public class Main {
     //TODO Use spotify api to view current track image, artist and track name https://github.com/spotify-web-api-java/spotify-web-api-java
     //TODO Improve youtube search to not play clip audio
     //TODO Fix seek cmd in MusicController
+    //TODO Add equalizer feature (bassboost, ect...) https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
     public static void main(String[] args) {
         Bot.setInstance(new Bot());
     }
