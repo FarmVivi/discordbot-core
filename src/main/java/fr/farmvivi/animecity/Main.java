dd6c0de3c8858f8145f7810e1c471e25a5cd971d
FarmVivi <farmvividev@gmail.com>
2021-09-21 11:24:09 +0200

    edit todo list

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -4,7 +4,7 @@ public class Main {
     //TODO Spotify playing support https://github.com/sedmelluq/lavaplayer/issues/519
     //TODO Use spotify api to view current track image, artist and track name https://github.com/spotify-web-api-java/spotify-web-api-java
     //TODO Improve youtube search to not play clip audio
-    //TODO Fix seek cmd in MusicController and TimeToIntCalculator
+    //TODO Fix seek cmd in MusicController
     public static void main(String[] args) {
         Bot.setInstance(new Bot());
     }
