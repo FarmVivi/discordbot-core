ea1ab7dd9a99caa38521a22acf23fc97c8848c75
FarmVivi <farmvividev@gmail.com>
2021-09-21 13:23:27 +0200

    todo

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -5,6 +5,7 @@ public class Main {
     //TODO Use spotify api to view current track image, artist and track name https://github.com/spotify-web-api-java/spotify-web-api-java
     //TODO Fix seek cmd in MusicController
     //TODO Add equalizer feature (bassboost, ect...) https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
+    //TODO Add features https://web.archive.org/web/20210911150725/https://rythm.fm/docs/features#player
     //TODO Don't leave channel if no more music to play
     public static void main(String[] args) {
         Bot.setInstance(new Bot());
