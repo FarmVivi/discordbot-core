75fda93bc428cf54a1d3eaa720e08168e4f3fb91
FarmVivi <farmvividev@gmail.com>
2021-09-22 20:07:56 +0200

    info to current cmd

--- src/main/java/fr/farmvivi/animecity/command/music/CurrentCommand.java
+++ src/main/java/fr/farmvivi/animecity/command/music/CurrentCommand.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class CurrentCommand extends Command {
     public CurrentCommand() {
         this.name = "current";
-        this.aliases = new String[] { "np" };
+        this.aliases = new String[] { "np", "info" };
     }
 
     @Override
