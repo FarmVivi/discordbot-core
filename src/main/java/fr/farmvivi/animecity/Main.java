5abf180da6adc407fd793348bbcec07a1337e5c6
FarmVivi <farmvividev@gmail.com>
2021-09-23 08:25:35 +0200

    todo list

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -7,6 +7,9 @@ public class Main {
     //TODO Add equalizer feature (bassboost, ect...) https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
     //TODO Add features https://web.archive.org/web/20210911150725/https://rythm.fm/docs/features#player
     //TODO Don't leave channel if no more music to play
+    //TODO Create a bot for testing
+    //TODO music commands only if connected as same voicechannel
+    //TODO Commande viewqueue
     public static void main(String[] args) {
         Bot.setInstance(new Bot());
     }
