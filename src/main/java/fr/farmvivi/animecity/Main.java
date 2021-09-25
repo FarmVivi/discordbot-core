08b4437b0c54ae1902856998c51b9765b95e271c
FarmVivi <farmvividev@gmail.com>
2021-09-25 23:02:16 +0200

    todo

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -2,6 +2,7 @@ package fr.farmvivi.animecity;
 
 public class Main {
     //TODO Fix seek cmd in MusicController
+    //TODO Fix leave cmd
     //TODO Add equalizer feature (bassboost, ect...) https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
     //TODO Add features https://web.archive.org/web/20210911150725/https://rythm.fm/docs/features#player
     //TODO Commande viewqueue
