67576dffa45b85588dd0f0e8832572c24042dd61
FarmVivi <farmvividev@gmail.com>
2021-09-26 10:44:34 +0200

    todo

--- src/main/java/fr/farmvivi/animecity/Main.java
+++ src/main/java/fr/farmvivi/animecity/Main.java
@@ -1,9 +1,11 @@
 package fr.farmvivi.animecity;
 
 public class Main {
-    //TODO Add equalizer feature (bassboost, ect...) https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
-    //TODO Add features https://web.archive.org/web/20210911150725/https://rythm.fm/docs/features#player
-    //TODO Commande help
+    // TODO Add equalizer feature (bassboost, ect...)
+    // https://github.com/sedmelluq/lavaplayer/blob/master/testbot/src/main/java/com/sedmelluq/discord/lavaplayer/demo/music/MusicController.java
+    // TODO Add features
+    // https://web.archive.org/web/20210911150725/https://rythm.fm/docs/features#player
+    // TODO Commande help
     public static void main(String[] args) {
         Bot.setInstance(new Bot());
     }
