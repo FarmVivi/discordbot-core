b380f05f66531c283500f66413940d5cad60f12d
FarmVivi <farmvividev@gmail.com>
2022-06-28 14:54:21 +0200

    Configuration update

--- src/main/java/fr/farmvivi/discordbot/Configuration.java
+++ src/main/java/fr/farmvivi/discordbot/Configuration.java
@@ -1,10 +1,20 @@
 package fr.farmvivi.discordbot;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Configuration {
+    private static final String ENV_PREFIX = "DISCORD_";
+
     public String jdaToken;
     public String spotifyId;
     public String spotifySecret;
@@ -13,50 +23,89 @@ public class Configuration {
     public String radioPath;
     public List<String> features;
 
+    private JsonObject jsonConfiguration;
+
     public Configuration() {
         try {
-            this.loadConfiguration();
-        } catch (Exception e) {
-            Bot.logger.error("Error loading configuration", e);
+            this.loadJsonConfiguration("." + File.separator + "config.json");
+        } catch (IOException e) {
+            Bot.logger.warn("Failed to load json configuration file", e);
+        }
+        try {
+            this.loadDefaultConfiguration();
+        } catch (ValueNotFoundException e) {
+            Bot.logger.error("Failed to load default configuration !", e);
+            System.exit(5);
         }
     }
 
-    public void loadConfiguration() throws Exception {
-        if (!validateConfiguration()) {
-            Bot.logger.error("Configuration isn't valid! Please just modify the default configuration !");
-            System.exit(1);
+    @SuppressWarnings("deprecation")
+    private void loadJsonConfiguration(String path) throws IOException {
+        Bot.logger.info("Configuration file is: " + path);
+
+        File configurationFile = new File(path);
+
+        if (!configurationFile.exists()) {
+            Bot.logger.warn("Configuration file don't exist");
             return;
         }
 
-        this.jdaToken = System.getenv("DISCORD_TOKEN");
-        this.spotifyId = System.getenv("SPOTIFY_ID");
-        this.spotifySecret = System.getenv("SPOTIFY_TOKEN");
-        this.cmdPrefix = System.getenv("CMD_PREFIX");
-        this.cmdAdmins = new ArrayList<>();
-        for (String admin : Arrays.asList(System.getenv("CMD_ADMINS").split(";")))
-            cmdAdmins.add(Long.parseLong(admin));
-        this.radioPath = System.getenv("RADIO_PATH");
-        this.features = new ArrayList<>();
-        for (String feature : Arrays.asList(System.getenv("FEATURES").split(";")))
-            features.add(feature);
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(configurationFile),
+                StandardCharsets.UTF_8)) {
+            this.jsonConfiguration = new JsonParser().parse(reader).getAsJsonObject();
+        }
     }
 
-    public boolean validateConfiguration() {
-        if (System.getenv("DISCORD_TOKEN") == null)
-            return false;
-        if (System.getenv("SPOTIFY_ID") == null)
-            return false;
-        if (System.getenv("SPOTIFY_TOKEN") == null)
-            return false;
-        if (System.getenv("CMD_PREFIX") == null)
-            return false;
-        if (System.getenv("CMD_ADMINS") == null)
-            return false;
-        if (System.getenv("RADIO_PATH") == null)
-            return false;
-        if (System.getenv("FEATURES") == null)
-            return false;
+    private void loadDefaultConfiguration() throws ValueNotFoundException {
+        this.jdaToken = getValue("TOKEN");
+        this.spotifyId = getValue("SPOTIFY_ID");
+        this.spotifySecret = getValue("SPOTIFY_TOKEN");
+        this.cmdPrefix = getValue("CMD_PREFIX");
+        this.cmdAdmins = new ArrayList<>();
+        for (String admin : getValues("CMD_ADMINS"))
+            cmdAdmins.add(Long.parseLong(admin));
+        this.radioPath = getValue("RADIO_PATH");
+        this.features = List.of("FEATURES");
+    }
 
-        return true;
+    public String getValue(String key) throws ValueNotFoundException {
+        String envKey = ENV_PREFIX + key.toUpperCase().replace("-", "_");
+        if (System.getenv(envKey) != null) {
+            return System.getenv(envKey);
+        }
+
+        if (jsonConfiguration != null && jsonConfiguration.has(key)) {
+            return jsonConfiguration.get(key).getAsString();
+        }
+
+        throw new ValueNotFoundException(
+                "Environment key '" + envKey + "' not found in environment variables or key '" + key
+                        + "' not found in configuration file");
+    }
+
+    public String[] getValues(String key) throws ValueNotFoundException {
+        String envKey = ENV_PREFIX + key.toUpperCase().replace("-", "_");
+        if (System.getenv(envKey) != null) {
+            return System.getenv(envKey).split(";");
+        }
+
+        if (jsonConfiguration != null && jsonConfiguration.has(key)) {
+            JsonArray jsonArray = jsonConfiguration.get(key).getAsJsonArray();
+            String[] values = new String[jsonArray.size()];
+            for (int i = 0; i < jsonArray.size(); i++) {
+                values[i] = jsonArray.get(i).getAsString();
+            }
+            return values;
+        }
+
+        throw new ValueNotFoundException(
+                "Environment key '" + envKey + "' not found in environment variables or key '" + key
+                        + "' not found in configuration file");
+    }
+
+    public static class ValueNotFoundException extends Exception {
+        public ValueNotFoundException(String message) {
+            super(message);
+        }
     }
 }
