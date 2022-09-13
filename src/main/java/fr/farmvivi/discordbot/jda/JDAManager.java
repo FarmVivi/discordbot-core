a36e5a417f480a58de490313b63e8db2b2346ec9
FarmVivi <farmvividev@gmail.com>
2022-09-13 23:15:00 +0200

    Fix errors

--- src/main/java/fr/farmvivi/discordbot/jda/JDAManager.java
+++ src/main/java/fr/farmvivi/discordbot/jda/JDAManager.java
@@ -4,14 +4,8 @@ import fr.farmvivi.discordbot.Bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.security.auth.login.LoginException;
 
 public class JDAManager {
-    public static final Logger logger = LoggerFactory.getLogger(JDAManager.class);
-
     private static final JDA jda = buildJDA();
 
     public static JDA getJDA() {
@@ -19,13 +13,8 @@ public class JDAManager {
     }
 
     private static JDA buildJDA() {
-        try {
-            return JDABuilder.createDefault(Bot.getInstance().getConfiguration().jdaToken)
-                    .enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
-        } catch (LoginException e) {
-            logger.error("Cannot build JDA !", e);
-        }
-
-        return null;
+        return JDABuilder.createDefault(Bot.getInstance().getConfiguration().jdaToken)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
     }
 }
