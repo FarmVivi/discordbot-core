559eb9d85f8c53f953fe23606b4570d55054e49a
FarmVivi <farmvividev@gmail.com>
2022-10-09 15:14:12 +0200

    Jda cache member roles

--- src/main/java/fr/farmvivi/discordbot/jda/JDAManager.java
+++ src/main/java/fr/farmvivi/discordbot/jda/JDAManager.java
@@ -4,6 +4,8 @@ import fr.farmvivi.discordbot.Bot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class JDAManager {
     private static final JDA jda = buildJDA();
@@ -14,10 +16,9 @@ public class JDAManager {
 
     private static JDA buildJDA() {
         return JDABuilder.createDefault(Bot.getInstance().getConfiguration().jdaToken)
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                .enableIntents(GatewayIntent.GUILD_MEMBERS)
-                .enableIntents(GatewayIntent.GUILD_MESSAGE_REACTIONS)
-                .enableIntents(GatewayIntent.GUILD_VOICE_STATES)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGE_REACTIONS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .setChunkingFilter(ChunkingFilter.ALL)
                 .build();
     }
 }
